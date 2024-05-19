4d988fe94d0067099325c367df8d8ae4389f5b07
yasmine0megahed <yasmine.aboulmaaty.megahed@gmail.com>
2024-05-19 11:33:05 +0300

    add:user and card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//component start
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
+//component end
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
